689c4ba52ce4429824c799432635d79ccb9e3459
zabarik21 <mityafara426@gmail.com>
2022-08-15 21:28:01 +0500

    Initial Commit

---
+++ CloudApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudApp
+//
+//  Created by Timofey on 15/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
